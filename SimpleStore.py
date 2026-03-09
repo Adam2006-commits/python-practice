ea5a6896f7ef5b41d5687d411ae3bc66e41c9611
Adam2006-commits <bobsimpson0805@gmail.com>
2026-03-08 21:31:03 -0400

    Add files via upload

---
+++ SimpleStore.py
@@ -0,0 +1,48 @@
+# Variables Section
+
+customer_name = "Alex"
+items_purchased = 12
+items_price = 20.50
+is_member = True
+
+print("CUSTOMER INFORMATION")
+print("Customer Name:", customer_name)
+print("Items Purchased:", items_purchased)
+print("Price per Item:", items_price)
+print("Store Member:", is_member)
+print()
+
+# Arthemtic Section
+
+print("ARTHMETIC CALCULATIONS")
+
+extra_items = 2
+total_items = items_purchased + extra_items
+print("Total Items After Adding More:", total_items)
+
+double_items = items_purchased * 2
+print("Items Doubled:", double_items)
+
+average_cost = items_price / 2
+print("Half Price Example:", average_cost)
+print()
+
+# Constants Section
+
+PI = 3.14159
+MAX_USERS = 100
+
+print("CONSTANT VALUES")
+print("PI:", PI)
+print("Maximum Users Allowed:", MAX_USERS)
+print()
+
+# meaningful expression section
+
+print("TOTAL PURCHASE CALCULATION")
+
+total_cost = items_purchased * items_price
+
+print("Items Purchased:". items_purchased)
+print("Item Price:", items_price)
+print("Total Cost:", total_cost)
